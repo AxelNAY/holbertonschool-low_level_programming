281530de4b2689b7fc6f98b236b1c50243ce94bb
AxelNAY <axelnay@sfr.fr>
2024-02-19 13:36:53 +0000

    sixth script

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+/*
+* File: 6-size.c
+* Auth: Axel Nay <8763@holbertonschool.com>
+*/
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+  sizeof(6-size.c);
+
+return (0);
+}
