7c57e1caba7227a90f4a903cc73de7350d96b95a
AxelNAY <axelnay@sfr.fr>
2024-02-19 13:25:33 +0000

    sixth script

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,20 +1,20 @@
-/*                                                                                                                                                                                  
- * File: 4-puts.c                                                                                                                                                                   
- * Auth: Axel Nay <8763@holbertonschool.com>                                                                                                                                        
- */
+/*                                                                                                                                                                                 
+* File: 5-printf.c                                                                                                                                                                  
+* Auth: Axel Nay <8763@holbertonschool.com>                                                                                                                                       
+*/
 
 #include <stdio.h>
 
-/**                                                                                                                                                                                 
- * main - Prints "Programming is like building a multilingual                                                                                                                       
- *                puzzle, followed by a new line.                                                                                                                                   
- *                                                                                                                                                                                  
- * Return: Always 0.                                                                                                                                                                
+/**                                                                                                                                                                  
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art, ");
 
 return (0);
 }
