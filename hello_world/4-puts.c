91b55742af3fb368a79338e66b25845f2b37c3be
AxelNAY <axelnay@sfr.fr>
2024-02-19 10:21:02 +0000

    fifth script

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-puts("\"Programming is like building a multilingual puzzle" + /n) | return(0)
+gcc 4-puts.c | puts("\"Programming is like building a multilingual puzzle" + /n) | return(0)
