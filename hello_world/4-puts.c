4e4d7e91148469ad1c9628093894bbc4fcadd965
AxelNAY <axelnay@sfr.fr>
2024-02-19 13:03:59 +0000

    fifth script

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/*
+Affichage de la phrase demander avec la fonction puts
+ */
+
 int main(void) {
   puts("\"Programming is like building a multilingual puzzle");
   return(0);
