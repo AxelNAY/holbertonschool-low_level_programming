ad8be84f8f4d64a206eca2caab37b49a3be1a038
AxelNAY <axelnay@sfr.fr>
2024-02-20 14:29:39 +0000

    fourth C Program

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+#include <stdio.h>
+
+/**
+* main - Prints all single digit numbers of base 10 starting from 0.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+int n = 0;
+
+while (n <= 9)
+{
+printf(n);
+n++;
+}
+putchar('\n');
+
+return (0);
+}
