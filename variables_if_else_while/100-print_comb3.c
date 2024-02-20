594056f16b88c5b36ce5a9596a63981c8d77d831
AxelNAY <axelnay@sfr.fr>
2024-02-20 20:24:47 +0000

    eleventh C Profram

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints all possible combinations of single-digit numbers.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+int n = 48;
+int m = 48;
+while (n <= 57)
+{
+if (m == 57) {
+putchar(n);
+m = 48;
+n++;
+}
+else
+putchar(n);
+putchar(m);
+if (n < 57)
+{
+putchar(32);
+}
+m++;
+}
+putchar('\n');
+
+return (0);
+}
