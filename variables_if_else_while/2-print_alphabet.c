209b3b465c11fa2d00c320bd690b06af5611d5cf
AxelNAY <axelnay@sfr.fr>
2024-02-20 10:25:16 +0000

    third C program

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int n = 65;
+
+while (n <= 90) {
+putchar(n);
+n++;
+}
+
+}
