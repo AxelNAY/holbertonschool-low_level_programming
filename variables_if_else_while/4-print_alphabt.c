9a9263017d23ee67f620f2b05a6e112df7ae0e82
AxelNAY <axelnay@sfr.fr>
2024-02-20 14:18:28 +0000

    fourth C Program

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase, except for q and e.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+int n = 97;
+
+while (n <= 122)
+{
+if ((n != 101) || (n != 113))
+putchar(n);
+
+n++;
+}
+putchar('\n');
+
+return (0);
+}
