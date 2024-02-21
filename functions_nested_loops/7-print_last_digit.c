c633071b0975beb5b39447aff14c792f53241459
AxelNAY <axelnay@sfr.fr>
2024-02-21 14:33:41 +0000

    eighth C Program

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* print_last_digit - Last digit.
+*
+* @c: Character to analyse.
+*
+* Return: Always a value.
+*/
+
+int print_last_digit(int c);
+{
+c = c % 10;
+putchar(c);
+return (c);
+}
