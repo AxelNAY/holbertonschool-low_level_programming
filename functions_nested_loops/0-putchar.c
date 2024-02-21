77f19959fa6bd63dea39a70c170faeaac16ed595
AxelNAY <axelnay@sfr.fr>
2024-02-21 09:33:08 +0000

    first C Program

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,7 +7,16 @@
 * Return: Always the value.
 */
 
-int _putchar(char c)
+int main (void)
 {
-return (write(1, &c, 1));
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
 }
