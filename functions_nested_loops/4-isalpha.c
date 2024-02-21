c503422a2b84ea6e9007f9ea76034fcc99f95154
AxelNAY <axelnay@sfr.fr>
2024-02-21 13:16:24 +0000

    fifth script

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+* _islower - return 1 if c is a letter and 0 otherwise,
+* followed by a new line.
+*
+* @c: Character to analyse.
+*
+* Return: 1 if success or 0 otherwise.
+*/
+
+int _isalpha(int c)
+{
+if ((c >= 97) && (c <= 122))
+return (1);
+else if ((c >= 65) && (c <= 90))
+return (1);
+else
+return (0);
+}
