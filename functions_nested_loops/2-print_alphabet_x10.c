555c1141af61241d2ce2a627c5f49062b5275abd
AxelNAY <axelnay@sfr.fr>
2024-02-21 10:13:20 +0000

    third C Program

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
-* print_alphabet - prints the alphabet in lowercase ten times, followed by a new line.
+* print_alphabet - prints the alphabet in lowercase ten times,
+* followed by a new line.
 */
 
 void print_alphabet_x10(void)
