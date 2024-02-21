ef411ed05aaf8d31f31a52d85a730a66c501e38b
AxelNAY <axelnay@sfr.fr>
2024-02-21 09:59:35 +0000

    third C Program

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* print_alphabet - prints the alphabet in lowercase ten times, followed by a new line.
+*/
+
+void print_alphabet_x10(void)
+{
+int n = 1;
+int m = 97;
+while (n <= 10)
+{
+while (n <= 122)
+{
+_putchar(m);
+m++;
+}
+m = 97;
+n++
+}
+_putchar('\n');
+}
