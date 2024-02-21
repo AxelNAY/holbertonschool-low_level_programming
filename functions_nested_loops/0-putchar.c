aa0da53dfcffc0eb492e0cc83a74f5f17c11948f
AxelNAY <axelnay@sfr.fr>
2024-02-21 08:50:29 +0000

    first C Program

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints _putchar.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+printf("_putchar");
+putchar('\n');
+
+return (0);
+}
