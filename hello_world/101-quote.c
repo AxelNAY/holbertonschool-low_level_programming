e04baeaff60e49539e3700033d17c5d5ef30437f
AxelNAY <axelnay@sfr.fr>
2024-02-22 21:16:30 +0000

    ninth C Program

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
 * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19 
 *                , followed by a new line. 
 *
-* Return: Always 0.
+* Return: Always 1.
 */
 
 int main(void)
