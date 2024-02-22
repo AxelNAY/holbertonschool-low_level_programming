4496066a94e2a8b0dd4ac4410aaa8ec23d2989c0
AxelNAY <axelnay@sfr.fr>
2024-02-22 10:51:19 +0000

    ninth C Program

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -3,29 +3,28 @@
 
 /**
 * jack_bauer - Display minutes.
-*
-* Return: Always a value.
 */
 
 void jack_bauer(void)
 {
 int h1 = 48;
 int h2 = 48;
- int m1 = 48;
- int m2 = 48;
-  while ((h1 <= 50) && (h2 <= 51))
-    {
+int m1 = 48;
+int m2 = 48;
+while ((h1 <= 50) && (h2 <= 51))
+{
 _putchar(h1);
 _putchar(h2);
 _putchar(58);
- while ((m1 <= 53) && (m2 <= 57))
-   {
-     _putchar(m1);
-     _putchar(m2);
-     m1++;
-     m2++;
-   }
- h1++;
- h2++;
-    }
+while ((m1 <= 53) && (m2 <= 57))
+{
+_putchar(m1);
+_putchar(m2);
+_putchar("\n");
+m1++;
+m2++;
+}
+h1++;
+h2++;
+}
 }
