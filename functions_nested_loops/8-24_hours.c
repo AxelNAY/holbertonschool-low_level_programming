00344673289222022faf53e6bcb39089c5638d9a
AxelNAY <axelnay@sfr.fr>
2024-02-22 13:25:18 +0000

    ninth C Program

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,24 +12,24 @@ int m1;
 int m2;
 while (hour <= 23)
 {
-  m1 = 48;
-  m2 = 48;
-  while (m1 <= 53)
-    {
-      _putchar(hour / 10 + 48);
-      _putchar(hour % 10 + 48);
-      _putchar(58);
-      _putchar(m1);
-      _putchar(m2);
-      _putchar('\n');
-      if (m2 == 57)
-	{
-	  m1++;
-	  m2 = 48;
-	}
-      else
-	m2++;
-    }
-  hour++;
+m1 = 48;
+m2 = 48;
+while (m1 <= 53)
+{
+_putchar(hour / 10 + 48);
+_putchar(hour % 10 + 48);
+_putchar(58);
+_putchar(m1);
+_putchar(m2);
+_putchar('\n');
+if (m2 == 57)
+{
+m1++;
+m2 = 48;
+}
+else
+m2++;
+}
+hour++;
 }
 }
