72800559949aee9752149d359fb23a3a075ba52e
AxelNAY <axelnay@sfr.fr>
2024-02-22 14:55:26 +0000

    twelveth C Program

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -4,65 +4,65 @@
 /**
 * print_to_98 - add the numbers.
 *
-* @c: number.
+* @n: number.
 *
 */
 
 void print_to_98(int n)
 {
- if (c == 98)
+if (n == 98)
 {
-_putchar(c / 10 + 48);
-_putchar(c % 10 + 48);
+_putchar(n / 10 + 48);
+_putchar(n % 10 + 48);
 }
 else
-    {
-while (c != 98)
 {
-if (c > 98)
+while (n != 98)
 {
-if (c < 10)
-_putchar(c + 48);
+if (n > 98)
+{
+if (n < 10)
+_putchar(n + 48);
 else
 {
-_putchar(c / 10 + 48);
-_putchar(c % 10 + 48);
-c--;
+_putchar(n / 10 + 48);
+_putchar(n % 10 + 48);
+n--;
 }
 _putchar(44);
 _putchar(33);
 }
-else if (c < 98)
+else if (n < 98)
 {
-if (c > 0)
+if (n > 0)
 {
-if (c < 10)
-_putchar(c + 48);
+if (n < 10)
+_putchar(n + 48);
 else
 {
-_putchar(c / 10 + 48);
-_putchar(c % 10 + 48);
+_putchar(n / 10 + 48);
+_putchar(n % 10 + 48);
 }
 }
-else if (c < 0)
-c = -c;
+else if (n < 0)
+n = -n;
 _putchar(45);
-if (c > -10)
-_putchar(c + 48);
+if (n > -10)
+_putchar(n + 48);
 else
 {
-_putchar(c / 10 + 48);
-_putchar(c % 10 + 48);
+_putchar(n / 10 + 48);
+_putchar(n % 10 + 48);
 }
-c++;
-}
-else if (c == 98)
-{
-_putchar(c / 10 + 48);
-_putchar(c % 10 + 48);
-}
+n++;
 _putchar(44);
 _putchar(33);
+}
+else if (n == 98)
+{
+_putchar(n / 10 + 48);
+_putchar(n % 10 + 48);
+}
 }
 }
 }
