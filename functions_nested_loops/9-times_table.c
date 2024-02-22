732ee06f84a3243378530b28f368d3d95fb3e293
AxelNAY <axelnay@sfr.fr>
2024-02-22 13:48:45 +0000

    tenth C Program

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,11 +13,17 @@ while (i < 10)
 int j = 0;
 while (j < 10)
 {
+  if ((i * j) < 10)
 _putchar((i * j) + 48);
+else
+{
+_putchar((i * j) / 10 + 48);
+_putchar((i * j) % 10 + 48);
+}
 if (j != 9)
 {
-_putchar(32);
 _putchar(44);
+_putchar(32);
 }
 j++;
 }
