c1dc0ab874692d22a934e7a53c7e401f740a897e
AxelNAY <axelnay@sfr.fr>
2024-02-23 11:31:30 +0000

    new

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - print the 50 first number of Fibonacci.
+*
+* Return: 0.
+*/
+
+int main(void)
+{
+int i = 0;
+int fib1 = 0;
+int fib2 = 0;
+int fib3 = 1;
+while (i <= 50)
+{
+fib2 = fib3;
+fib3 = fib3 + fib1;
+fib1 = fib2;
+printf("%d", fib2);
+if (i < 50)
+{
+putchar(44);
+putchar(32);
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
