9f910855d7fdbada5558dc0e06d99f799da00ed8
AxelNAY <axelnay@sfr.fr>
2024-02-23 12:00:51 +0000

    sixteenth C Program

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -8,18 +8,16 @@
 
 int main(void)
 {
-int i = 1;
 long int fib1 = 0;
 long int fib2 = 0;
 long int fib3 = 1;
 long int sum = 0;
-while (i <= 50)
+while (sum < 4000000)
 {
 fib2 = fib3;
 fib3 = fib3 + fib1;
 fib1 = fib2;
 sum = sum + fib3;
-i++;
 }
 printf("%ld", sum);
 putchar('\n');
