323c579a4a04d07b3ea885da9e2806c834471ed7
AxelNAY <axelnay@sfr.fr>
2024-02-23 12:33:18 +0000

    seventeenth C Program

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 int i = 1;
-long int fib1 = 0;
-long int fib2 = 0;
-long int fib3 = 1;
+long fib1 = 0;
+long fib2 = 0;
+long fib3 = 1;
 while (i <= 98)
 {
 fib2 = fib3;
 fib3 = fib3 + fib1;
 fib1 = fib2;
-printf("%ld", fib3);
+printf("%lu", fib3);
 if (i < 98)
 {
 putchar(44);
