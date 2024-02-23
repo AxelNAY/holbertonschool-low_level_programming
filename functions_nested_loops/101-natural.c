94e5e7f9a5996d795c11b67bcb5a922da2474c4d
AxelNAY <axelnay@sfr.fr>
2024-02-23 09:58:23 +0000

    thirteenth C Program

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* main - print all the multiple of 3 and 5.
+*/
+
+int main(void)
+{
+int i = 0;
+while (i <= 1024)
+{
+if ((i % 3) == 0)
+{
+_putchar(i + 48);
+  _putchar('\n');
+}
+else if ((i % 5) == 0)
+{
+_putchar(i + 48);
+_putchar('\n');
+}
+i++;
+}
+}
