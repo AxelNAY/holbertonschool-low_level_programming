c2d189083300a7f9395209bde7d83e60bb77e07e
AxelNAY <axelnay@sfr.fr>
2024-02-23 08:42:15 +0000

    thirteenth C Program

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* print_times_table - display n table n time.
+*
+* @n: number for the table.
+*
+* Return: return the sum
+*/
+
+void print_times_table(int n);
+{
+int i = 0;
+if ((n > 15) || (n < 0))
+{
+while (i < n)
+{
+int j = 0;
+while (j < n)
+{
+if ((i * j) < 10)
+{
+if (j > 0)
+_putchar(32);
+_putchar((i * j) + 48);
+}
+else
+{
+_putchar((i * j) / 10 + 48);
+_putchar((i * j) % 10 + 48);
+}
+if (j < n)
+{
+_putchar(44);
+_putchar(32);
+}
+j++;
+}
+_putchar('\n');
+i++;
+}
+}
+}
