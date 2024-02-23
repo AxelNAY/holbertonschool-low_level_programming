3720729899f66505ab42594788ce1fa0a3fac07b
AxelNAY <axelnay@sfr.fr>
2024-02-23 12:46:32 +0000

    seventeenth C Program

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -9,9 +9,11 @@
 int main(void)
 {
 int i = 1;
-unsigned long fib1 = 0;
-unsigned long fib2 = 1;
-unsigned long sum;
+unsigned long fib1 = 0, fib2 = 1, sum;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+unsigned long half1, half2;
+
+
 while (i <= 98)
 {
 fib2 = sum;
@@ -26,12 +28,28 @@ putchar(32);
 i++;
 }
 
+fib1_half1 = fib1 / 10000000000;
+fib2_half1 = fib2 / 10000000000;
+fib1_half2 = fib1 % 10000000000;
+fib2_half2 = fib2 % 10000000000;
 
-
-
-
-
- 
-putchar('\n');
+for (i = 93; i < 99; i++)
+{
+half1 = fib1_half1 + fib2_half1;
+half2 = fib1_half2 + fib2_half2;
+if (fib1_half2 + fib2_half2 > 9999999999)
+{
+half1 += 1;
+half2 %= 10000000000;
+}
+printf("%lu%lu", half1, half2);
+if (i < 98)
+printf(", ");
+fib1_half1 = fib2_half1;
+fib1_half2 = fib2_half2;
+fib2_half1 = half1;
+fib2_half2 = half2;
+}
+printf("\n");
 return (0);
 }
