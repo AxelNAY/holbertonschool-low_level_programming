0b48a52fc2f3885a5011dc941cb4f2c739e2ef54
AxelNAY <axelnay@sfr.fr>
2024-02-23 11:42:48 +0000

    sixteenth C Program

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-int i = 0;
-int fib1 = 0;
-int fib2 = 0;
-int fib3 = 1;
+int i = 1;
+long int fib1 = 0;
+long int fib2 = 0;
+long int fib3 = 1;
 while (i <= 50)
 {
 fib2 = fib3;
 fib3 = fib3 + fib1;
 fib1 = fib2;
-printf("%d", fib2);
+printf("%ld", fib3);
 if (i < 50)
 {
 putchar(44);
