f7730cd0a65a1a1d56070cdf79feb74de3510699
AxelNAY <axelnay@sfr.fr>
2024-02-26 14:00:51 +0000

    tenth C Program

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Display the number from 0 to 100
+* Except multiplier of 3 and 5.
+*
+* Return: Always return 0
+*/
+
+int main(void)
+{
+int i = 1;
+while (i < 101)
+{
+if (((i % 3) == 0) && ((i % 5) == 0))
+printf("FizzBuzz");
+if ((i % 3) == 0)
+printf("Fizz");
+else if ((i % 5) == 0)
+printf("Buzz");
+else
+printf("%d", i);
+if (i < 100)
+_putchar(32);
+}
+_putchar('\n');
+return (0);
+}
