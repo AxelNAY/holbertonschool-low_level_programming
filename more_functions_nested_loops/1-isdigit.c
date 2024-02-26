9ce4bf8f8a486bd55dfe8d2b4ca724a30180a55c
AxelNAY <axelnay@sfr.fr>
2024-02-26 09:28:26 +0000

    second C Program

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isdigit - checks numbers
+*
+* @c: Character to analyse.
+*
+* Return: 1 if success or 0 otherwise.
+*/
+
+int _isdigit(int c)
+{
+if ((c >= 48) && (c <= 57))
+return (1);
+else
+return (0);
+}
