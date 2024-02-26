10ed1085d669db458b0780804d0c2ce4bdb75ef7
AxelNAY <axelnay@sfr.fr>
2024-02-26 10:33:23 +0000

    sixth C Program

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* more_numbers - display numbers from 0 to 14
+*/
+
+void more_numbers(void)
+{
+int c1 = 48;
+int c2 = 48;
+while ((c != 49) && (c2 < 53))
+{
+_putchar(c1);
+if ((c == 49) && (c2 != 48))
+{
+_putchar(c2);
+c2++;
+}
+else if (c == 57)
+{
+c1 = 49;
+c2++;
+}
+else
+c1++;
+}
+_putchar('\n');
+}
