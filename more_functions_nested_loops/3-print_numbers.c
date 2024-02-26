fb5990e08529893bad04268c5ff2b66702233c22
AxelNAY <axelnay@sfr.fr>
2024-02-26 09:44:13 +0000

    fourth C Program

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_numbers - display numbers
+*/
+
+void print_numbers(void)
+{
+int c = 48;
+while (c < 58)
+{
+putchar(c);
+}
+putchar('\n');
+}
