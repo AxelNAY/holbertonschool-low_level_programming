0a9e9e3a25ab84da19893ea382e4c8432a61ea40
AxelNAY <axelnay@sfr.fr>
2024-02-26 14:50:26 +0000

    twelveth C Program

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - print a square
+*
+* Return: number of times for triangle
+*/
+
+int main(void)
+{
+int i = 0;
+double num = 612852475143;
+double res;
+while ((num % 2) == 0)
+{
+num = num / 2;
+res = num;
+}
+}
