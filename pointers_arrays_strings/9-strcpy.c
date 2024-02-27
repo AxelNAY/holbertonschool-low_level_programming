47494f29406ec41ea66c807e31ae8dda63a36872
AxelNAY <axelnay@sfr.fr>
2024-02-27 20:53:16 +0000

    tenth C Program

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,23 +1,20 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 
 /**
-* _strcpy - copy all the data from src and dest.
-*
-* @dest: destination
-*
-* @src: source
-*
-* Return: retun the copy pointer.
-*/
-
+ * _strcpy - Copies a string pointed to by @src, including the
+ *           terminating null byte, to a buffer pointed to by @dest.
+ * @dest: A buffer to copy the string to.
+ * @src: The source string to copy.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
 char *_strcpy(char *dest, const char *src)
 {
-int i = 0;
-while (src[i])
+int index = 0;
+while (src[index])
 {
-dest[i] = src[i];
-i++;
+dest[index] = src[index];
+index++;
 }
 return (dest);
 }
