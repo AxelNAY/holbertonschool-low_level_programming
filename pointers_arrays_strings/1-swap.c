e8fce12732dc0e599c23877a69527a03757a403e
AxelNAY <axelnay@sfr.fr>
2024-02-27 09:10:37 +0000

    second C Program

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* swap_int - swap the two value of the variables
+*
+* @a: pointer a
+*
+* @b: pointer b
+*/
+
+void swap_int(int *a, int *b)
+{
+int *p = &a;
+*a = &b;
+*b = &p;
+}
