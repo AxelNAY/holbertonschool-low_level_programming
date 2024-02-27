8b5a541750979dd173f10cb99b3c60820de76e23
AxelNAY <axelnay@sfr.fr>
2024-02-27 14:17:35 +0000

    eighth C Program

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* puts_half - print half of a string
+*
+* @str: pointer str
+*/
+void puts_half(char *str);
+{
+int len = 0;
+int i = 0;
+while (s[len])
+len++;
+for (i = 0; i <= len / 2; i++)
+_putchar(s[i]);
+_putchar('\n');
+}
