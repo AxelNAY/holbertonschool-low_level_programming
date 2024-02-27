5a490689c81c6c3f42b4d06243cf83391807a80b
AxelNAY <axelnay@sfr.fr>
2024-02-27 13:32:27 +0000

    sixth C Program

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* rev_string - reverse a string
+*
+* @s: pointer s
+*/
+
+void rev_string(char *s)
+{
+int len = 0;
+int i = 0;
+char *p;
+while (s[len])
+len++;
+for (i = len - 1; i >= 0; i--)
+*p = s[i];
+}
