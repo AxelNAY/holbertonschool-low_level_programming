1fc6ff329ff9b185bb36cf29092210c369d3a567
AxelNAY <axelnay@sfr.fr>
2024-02-27 09:31:44 +0000

    third C Program

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen - return the length of the pointer
+*
+* @s: pointer s
+*
+* Return: return the length
+*/
+
+int _strlen(char *s)
+{
+  return (sizeof(*s))
+}
