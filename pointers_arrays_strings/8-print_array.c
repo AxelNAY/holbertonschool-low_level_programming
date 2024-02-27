4f9a488949cbbb1966eb0120428ad5ab90a2e74a
AxelNAY <axelnay@sfr.fr>
2024-02-27 20:35:41 +0000

    tenth C Program

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints an inputted number of elements
- *               of an array of integers.
- * @a: The array of integers.
- * @n: The number of elements to be printed.
- */
+* print_array - Prints all variables of the pointer
+* @a: pointer a
+* @n: array
+*/
 void print_array(int *a, int n)
 {
-int index;
-for (index = 0; index < n; index++)
+int i;
+for (i = 0; i < n; i++)
 {
-printf("%d", a[index]);
-if (index == n - 1)
+printf("%d", a[i]);
+if (i == n - 1)
 continue;
 printf(", ");
 }
---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* *_strcpy - copy all the data from src and dest.
+*
+* @dest: destination
+*
+* @src: source
+*/
+
+char *_strcpy(char *dest, char *src);
+{
+int len = 0;
+int i;
+while (src[len])
+len++;
+for (i = 0; i < len; i++;)
+{
+dest = src[i];
+}
+return (dest);
+}
