cb5d903ae1987b49663879090a257192508a77c8
AxelNAY <axelnay@sfr.fr>
2024-02-27 13:27:10 +0000

    fifth C Program

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,10 @@
 void print_rev(char *s)
 {
 int len = 0;
-int i;
-while (*s++)
+int i = 0;
+while (s[len])
 len++;
-for (i = len + 1; i >= 0; s--)
-{
-_putchar(*s);
-i--;
-}
+for (i = len - 1; i >= 0; i--)
+_putchar(s[i]);
 _putchar('\n');
 }
