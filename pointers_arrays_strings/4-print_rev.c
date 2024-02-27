ae87bbdfedf57670a7126c64d0a97362ca0b6db5
AxelNAY <axelnay@sfr.fr>
2024-02-27 10:28:27 +0000

    fourth C Program

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_rev - print a string in reverse
+* follow by a new line.
+*
+* @s: pointer s
+*/
+
+void print_rev(char *s)
+{
+while (*str--)
+_putchar(*str);
+_putchar('\n');
+}
