4960c8d6a13f4e211870d3c5bb0c26753cc60b5a
AxelNAY <axelnay@sfr.fr>
2024-02-28 13:03:45 +0000

    eleventh commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,14 +12,13 @@ int _atoi(char *s)
 int neg_pos = 1;
 unsigned int n = 0;
 
-while (*s)
-{
+do {
 if (*s == '-')
 neg_pos *= -1;
-else if ((*s >= '0') && (*s <= '9'))
+else if (*s >= '0' && *s <= '9')
 n = (n * 10) + (*s - '0');
 else if (n > 0)
 break;
-}
+} while (*s++);
 return (n * neg_pos);
 }
