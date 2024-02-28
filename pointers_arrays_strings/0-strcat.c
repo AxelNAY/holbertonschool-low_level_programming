62791f6fba2e979fd53e2a6529039406c1c1e7e4
AxelNAY <axelnay@sfr.fr>
2024-02-28 13:27:28 +0000

    first C Program

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* *_strcpy - copy all the data from src and dest.
+*
+* @dest: destination
+*
+* @src: source
+*
+* Return: retun the copy pointer.
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int i;
+int leng = 0;
+char *newdest = *dest;
+
+while (*src++);
+leng++; 
+for (i = 0; i <= (len - 1); i++)
+{
+dest[leng] = src[i];
+}
+*dest = '\0';
+return (newdest);
+}
