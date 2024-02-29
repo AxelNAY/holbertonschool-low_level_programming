a08d8ecd72c77f193aa9e2f1d38ced3eac5766f5
AxelNAY <axelnay@sfr.fr>
2024-02-29 21:52:15 +0000

    seventh C Program

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* cap_string - capitalizes all words of a string.
+*
+* @c: pointer c
+*
+* Return: result
+*/
+
+char *cap_string(char *c);
+{
+char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+return(*c);
+}
