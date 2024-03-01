0d40d4f9292bbfc35fe66a41b6cfbf8898c2be00
AxelNAY <axelnay@sfr.fr>
2024-03-01 13:45:21 +0000

    eighth C Program

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -14,18 +14,17 @@ char *leet(char *c)
 int i;
 char let[10] = {'a', 'A', 'E', 'e', 'O', 'o'
 , 'T', 't', 'L', 'l'};
- char num[10] = {'4', '4', '3', '3', '0', '0'
-   , '7', '7', '1', '1'};
+char num[10] = {'4', '4', '3', '3', '0', '0'
+, '7', '7', '1', '1'};
 
  for (i = 0; c[i] != '\0'; i++)
    {
-  int j;
-  int k;
-  for (j = 0, k = 0; let[j] != '\0'; j++, k++)
-   {
-     if (c[i] == let[j])
-       c[i] = num[k];
+     int j;
+     for (j = 0; j <= 10; j++)
+       {
+	 if (c[i] == let[j])
+	   c[i] = num[j];
+       }
    }
-   }
-return (c);
+ return (c)
 }
