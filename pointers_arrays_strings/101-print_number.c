9e56f3d0e077d04652469128c6c0df7833444a66
AxelNAY <axelnay@sfr.fr>
2024-03-01 21:00:26 +0000

    tenth C Program

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -9,32 +9,35 @@
 
 void print_number(int n)
 {
-int i;
-unsigned int dec;
-int mult = 1;
 unsigned int abs;
+int mult = 1;
+unsigned int abSCount;
+int i;
 int c = 0;
 if (n == 0)
-putchar(48);
-else if (n < 0)
 {
-putchar(48);
-n = n * -1;
-n = n + 1;
+_putchar('0');
+}
+if (n < 0)
+{
+_putchar('-');
+n += 1;
+n *= -1;
+n++;
 }
 abs = n;
-dec = n;
-while (dec > 0)
+abSCount = n;
+while (abSCount > 0)
 {
-dec = dec / 10;
+abSCount /= 10;
 c++;
 }
 for (i = 0; i < c - 1; i++)
-mult = mult * 10;
+mult *= 10;
 for (i = 0; i < c; i++)
 {
-_putchar((abs / mult) + 48);
+_putchar((abs / mult) + '0');
 abs = abs % mult;
-mult = mult / 10;
+mult /= 10;
 }
 }
