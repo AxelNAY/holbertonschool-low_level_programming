7fd1ef6683a7b9694d3fd02043729491545c23f1
AxelNAY <axelnay@sfr.fr>
2024-03-01 08:40:24 +0000

    seventh C Program

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,16 @@
 
 char *cap_string(char *c);
 {
+int i;
 char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-
+for (i = 0; c[i] != '\0'; i++)
+  {
+    int j;
+    for (j = 0; sep[j] != '\0', j++)
+      {
+	if (c[i] == sep[j])
+      c[i] = c[i] - 32;
+      }
+  }
 return(*c);
 }
