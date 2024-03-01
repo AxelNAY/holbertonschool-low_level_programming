bbeeaaeacd041abcf6f8bf67301bea5de95f8a0d
AxelNAY <axelnay@sfr.fr>
2024-03-01 14:51:22 +0000

    tenth C Program

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -12,12 +12,12 @@
 char *rot13(char *c)
 {
 int i;
-char alph[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'
+char alph[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'
    , 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S'
    , 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd'
    , 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'
    , 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-char rot[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'
+char rot[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'
    , 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F'
    , 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q'
    , 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b'
@@ -25,7 +25,7 @@ char rot[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'
 for (i = 0; c[i]; i++)
 {
 int j;
-for (j = 0; j < 52; j++)
+for (j = 0; j <= 52; j++)
 {
 if (c[i] == alph[i])
 c[i] = rot[j];
