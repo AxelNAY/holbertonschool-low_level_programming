f6a40a0ec7c1f2699f8c2e55f0acba92cd742553
AxelNAY <axelnay@sfr.fr>
2024-03-01 14:49:28 +0000

    tenth C Program

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -12,21 +12,23 @@
 char *rot13(char *c)
 {
 int i;
-char maj[26] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'
+char alph[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'
+   , 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S'
+   , 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd'
+   , 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'
+   , 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char rot[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'
    , 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F'
-  , 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
-char min[26] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u'
-   , 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f'
-  , 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+   , 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q'
+   , 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b'
+   , 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 for (i = 0; c[i]; i++)
 {
 int j;
-for (j = 0; j < 26; j++)
+for (j = 0; j < 52; j++)
 {
-if (c[i] == maj[j])
-c[i] = maj[j];
-else
-c[i] = min[j];
+if (c[i] == alph[i])
+c[i] = rot[j];
 }
 }
 return (c);
