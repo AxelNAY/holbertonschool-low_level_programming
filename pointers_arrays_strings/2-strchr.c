095a6d8c9c85536c15a3c2a6f36359d7bd3a9fde
AxelNAY <axelnay@sfr.fr>
2024-03-04 10:52:14 +0000

    third C Program

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,13 @@
 char *_strchr(char *s, char c)
 {
 unsigned int i;
- int res = -1;
+int res = -1;
 for (i = 0; s[i] != '\0'; i++)
 {
-  if (s[i] == c && res == -1)
-    res = i;
+if (s[i] == c && res == -1)
+res = i;
 }
- if (res != -1)
-   return (&s[res]);
+if (res != -1)
+return (&s[res]);
 return ('\0');
 }
