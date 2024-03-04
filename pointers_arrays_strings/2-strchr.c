8533f1197edbd9c8c7d5b9aa4915b390e8fa0289
AxelNAY <axelnay@sfr.fr>
2024-03-04 10:16:52 +0000

    third C Program

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+* _strchr - locates a character in a string.
+* @s: string.
+* @c: character.
+* Return: pointer s.
+*/
+
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+ char res = '0';
+for (i = 0; s[i] != '\0'; i++)
+{
+  if (s[i] == c)
+    res = s[i];
+}
+ if (res != '0')
+   return (res);
+ else
+   return (NULL);
+}
