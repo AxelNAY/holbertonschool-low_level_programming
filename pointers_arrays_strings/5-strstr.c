1c411a26f455dab642a770173e95b82d137d7ba3
AxelNAY <axelnay@sfr.fr>
2024-03-05 10:51:26 +0000

    sixth C Program

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,34 +1,25 @@
 #include "main.h"
+
 /**
- * _strstr - function that locates a substring
- * @haystack: string to search in
- * @needle: string to look for
- * Return: return a pointer
+ * _strstr - searches string for a substring
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: substring if find
  */
+
 char *_strstr(char *haystack, char *needle)
 {
 int i;
-int j;
 
-for (j = 0; needle[j] != '\0'; j++)
-{
-;
-}
 while (*haystack)
 {
-	if (j > 0 && haystack[j - 1] == '\0')
-	{
-		return ('\0');
-	}
-	while (needle[i] != '\0' && haystack[i] == needle[i])
-	{
-		i++;
-	}
-	if (needle[i] == '\0')
-	{
-		return (haystack);
-	}
-	haystack++;
+for (i = 0; *(needle + i); i++)
+if (*(needle + i) != *(haystack + i))
+break;
+if (!*(needle + i))
+return (haystack);
+haystack++;
 }
 return ('\0');
 }
