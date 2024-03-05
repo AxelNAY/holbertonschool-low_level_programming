af2b57ba2f097c92e3517b7c1121e3c99ce7538b
AxelNAY <axelnay@sfr.fr>
2024-03-05 10:08:49 +0000

    fifth C Program

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -9,16 +9,13 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-int i, j;
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] != 32)
+int j;
+for (; *s; s++)
 {
 for (j = 0; accept[j] != '\0'; j++)
 {
-if (s[i] == accept[j])
-return (accept);
-}
+if (*s == accept[j])
+return (s);
 }
 }
 return ('\0');
