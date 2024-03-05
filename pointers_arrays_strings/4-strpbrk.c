4ca05ea409ff31e883a175b70a39bf21c91c69eb
AxelNAY <axelnay@sfr.fr>
2024-03-05 09:46:29 +0000

    fifth C Program

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strpbrk - searches a string for any of a set of bytes.
+* @s: string.
+* @accept: substring.
+* Return: pointer s.
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] != 32)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (accept[j] == s[i])
+return (s);
+}
+}
+}
+return ('\0');
+}
