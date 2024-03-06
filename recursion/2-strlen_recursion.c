b5ad81735d34ebab3cbcfd9f53bed1ee8a3873a1
AxelNAY <axelnay@sfr.fr>
2024-03-06 10:21:04 +0000

    third C Program

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- *  - returns the length of a string.
+ * _strlen_recursion - returns the length of a string.
  * @s: string.
  */
 
 int _strlen_recursion(char *s)
 {
-int len = 0;
+int len;
 if (*s == '\0')
 {
-return (len);
+return (0);
 }
+else
+len = 0;
 while (*s++)
 len++;
-_strlen_recursion(s + 1);
+return (len + _strlen_recursion(s + 1));
 }
