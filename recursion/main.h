d57116572ca60d253985c82f96f0f8d03c4d50a0
AxelNAY <axelnay@sfr.fr>
2024-03-06 21:24:14 +0000

    seventh C Program

--- recursion/main.h
+++ recursion/main.h
@@ -9,6 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int operation(int n, int root);
+int prime(int n, int r);
 int is_prime_number(int n);
 
 #endif
