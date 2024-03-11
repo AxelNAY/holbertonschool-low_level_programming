1da17412ab105c862ae528b1608d80caf9ca6651
AxelNAY <axelnay@sfr.fr>
2024-03-11 14:49:34 +0000

    third C Program

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,27 +3,35 @@
 #include <stdio.h>
 
 /**
-* _strdup - returns a pointer to a newly allocated space in memory,
-* which contains a copy of the string given as a parameter.
-* @str: string
-* Return: pointer.
+* str_concat - concatenates two strings.
+* @s1: string 1.
+* @s2: string 2.
+* Return: pointer or NULL.
 */
 
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	int i;
-	int len = 0;
+	int i, j;
+	int len1 = 0;
+	int len2 = 0;
+	int slen = 0;
 	char *s;
 
-	if (str == 0)
+	if ((s1 == 0) || (s2 == 0))
 		return (NULL);
-	while (str[len] != '\0')
-		len++;
-	s = malloc(len * sizeof(char));
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+	slen = len1 + len2;
+	s = malloc((slen + 1) * sizeof(char));
 	if (s == 0)
 		return (NULL);
-	for (i = 0; i <= len - 1; i++)
-		s[i] = str[i];
-	s[len] = '\0';
+	for (i = 0; i <= len1 - 1; i++)
+		s[i] = s1[i];
+	j = i;
+	for (i = 0; i <= len2 - 1; i++)
+		s[j] = s2[i];
+	s[slen] = '\0';
 	return (s);
 }
