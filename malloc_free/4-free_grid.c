9edb7a9e43c2743a73f95baa89767c6bfb31845e
AxelNAY <axelnay@sfr.fr>
2024-03-12 09:33:38 +0000

    fifth C Program

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* alloc_grid - returns a pointer to a 2 dimensional array of integers.
+* @grid: return of the function alloc_grid.
+* @height: height.
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
