3158ff8b1c421864dfff1a2f95db4bd53d32411e
AxelNAY <axelnay@sfr.fr>
2024-03-12 12:54:33 +0000

    second C Program

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,27 +16,36 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = 0;
 	char *s;
 
-	if ((s1 == 0) || (s2 == 0))
+	if ((s1 == NULL) || (s2 == NULL))
 	{
 		s1 = "";
 		s2 = "";
 	}
+
 	while (s1[len1] != '\0')
 		len1++;
+	
 	while (s2[len2] != '\0')
 		len2++;
+	
 	if (len2 > n)
 		len2 = n;
+	
 	s = malloc((len1 + len2 + 1) * sizeof(char));
-	if (s == 0)
+	
+	if (s == NULL)
 		return (NULL);
+	
 	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
+	
 	for (j = 0; j < n; j++)
 	{
 		s[i] = s2[j];
 		i++;
 	}
+	
 	s[i] = '\0';
+	
 	return (s);
 }
