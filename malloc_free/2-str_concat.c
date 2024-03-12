a4c36480c2c3e9cd6a8a60855fa502fb406b4e26
AxelNAY <axelnay@sfr.fr>
2024-03-12 13:42:05 +0000

    third C Program

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,15 +16,20 @@ char *str_concat(char *s1, char *s2)
 	int len2 = 0;
 	char *s;
 
-	if ((s1 == 0) || (s2 == 0))
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
 	s = malloc((len1 + len2 + 1) * sizeof(char));
-	if (s == 0)
+	if (s == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
 	for (j = 0; s2[j] != '\0'; j++)
