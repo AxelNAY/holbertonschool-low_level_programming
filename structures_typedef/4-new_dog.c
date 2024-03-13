08cca9254c4f4dd4bc5d7a8a96422c02fd9d5771
AxelNAY <axelnay@sfr.fr>
2024-03-13 13:40:28 +0000

    third C program

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,56 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * new_dog - prints a struct dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i = 0, j = 0, k;
+	dog_t *d;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	
+	d = malloc(sizeof(dog_t));
+	
+	if (d == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	d->name = malloc(i * sizeof(d->name));
+	d->age = age;
+	d->owner = malloc(j * sizeof(d->owner));
+	
+	if (d->name == NULL)
+        {
+		free(d);
+                free(d->name);
+                return (NULL);
+        }
+	if (d->owner == NULL)
+        {
+                free(d);
+                free(d->owner);
+                return (NULL);
+        }
+
+	for (k = 0; k <= i; k++)
+		d->name[k] = name[k];
+
+        d->age = age;
+	
+	for (k = 0; k <= j; k++)
+                d->owner[k] = owner[k];
+
+	return (d);
+
+}
