73862a202363331860c1acc44ea89b4c43c6bb4d
AxelNAY <axelnay@sfr.fr>
2024-03-15 13:56:13 +0000

    seventh C program

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -10,17 +10,20 @@
 
 char **strtow(char *str)
 {
-	int i, j, w = 0, k = 0, len = 0;
-	char **s;
+	int i, l, w = 0, k = 0, len = 0, firstlet, lastlet;
+	char **s, *word;
 
 	if (str == NULL)
 		return (NULL);
 	while (str[len])
 	{
-		if (str[len] != ' ')
-			i = 0;
-		else if (i = 0)
+		if (str[len] == ' ')
+			k = 0;
+		else if (k == 0)
+		{
+			k = 1;
 			w++;
+		}
 		len++;
 	}
 
@@ -28,17 +31,27 @@ char **strtow(char *str)
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; str[i]; i++)
+	for (i = 0; i < len; i++)
 	{
-		if (str[i++] != ' ' || str[i] != ' ')
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			s[i] = malloc(len + 1 * sizeof(char));
-			for (j = 0; s[i][j]; j++)
+			if (l)
 			{
-				s[i][j] = str[k];
+				lastlet = i;
+				word = malloc(l + 1 * sizeof(char));
+				if (word == NULL)
+					return (NULL);
+				for (; firstlet < lastlet; firstlet++)
+					*word++ = str[firstlet];
+				*word = '\0';
+				s[k] = word - l;
 				k++;
+				l = 0;
 			}
 		}
+		else if (l++ == 0)
+			firstlet = i;
 	}
+
 	return (s);
 }
