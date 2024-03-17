f2f8c2e5ad6bf274a4ce3d6b3e35beb3ef1fd7c3
AxelNAY <axelnay@sfr.fr>
2024-03-17 21:34:45 +0000

    fourth C program

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -12,10 +12,17 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *s;
+	char *s, *old_ptr;
 	unsigned int i;
 
-	s = malloc(new_size)
+	s = malloc(new_size);
+	if (s == NULL)
+		return (NULL);
+
+	old_ptr = ptr;
+	if (s == NULL)
+		return (NULL);
+
 	if (ptr == NULL)
 		return (malloc(new_size));
 	if (new_size == old_size)
@@ -25,14 +32,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free (ptr);
 		return (NULL);
 	}
-	        if (new_size > old_size)
-        {
-                for (i = 0; i < old_size; i++)
-                        s[i] = ptr[i];
-        }
-        if (new_size < old_size)
-        {
-                for (i = 0; i < new_size; i++)
-                        ptr1[i] = ptr[i];
-        }
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			s[i] = old_ptr[i];
+	}
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			s[i] = old_ptr[i];
+	}
+	
+	free(ptr);
+	return (s);
 }
