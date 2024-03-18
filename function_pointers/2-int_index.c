30a27b878579fb4f5e58ff0c3cc496ad562de713
AxelNAY <axelnay@sfr.fr>
2024-03-18 13:48:59 +0000

    third C program

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -6,17 +6,23 @@
  * @array: array.
  * @size: size.
  * @cmp: pointer.
+ * Return: result.
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, res;
+	int i, res = 0;
+
+	if (size <= 0)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			res++;
 	}
-
+	
+	if (res == 0)
+		return (-1);
 	return (res);
 }
