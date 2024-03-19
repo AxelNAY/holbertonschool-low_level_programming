1246ecf319f6db1f77bb7fa8083ec33595e98edf
AxelNAY <axelnay@sfr.fr>
2024-03-19 13:50:01 +0000

    second C program

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,11 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator != NULL)
 	{
-		va_start(ap, separator);
+		va_start(ap, n);
 		for (i = 0; i < n; i++)
 		{
-			sum += va_arg(ap, int);
-			if (i <
+			printf("%d", va_arg(ap, int));
+			if (i < n - 1)
+				printf("%s", separator);
 		}
 		va_end(ap);
 	}
