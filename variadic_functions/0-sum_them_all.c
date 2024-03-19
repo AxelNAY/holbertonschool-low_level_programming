73fd5dea13310d1e3457e46ddd120226b3be6e5b
AxelNAY <axelnay@sfr.fr>
2024-03-19 13:29:43 +0000

    first C program

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -17,9 +17,8 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 
 	va_start(ap, n);
-
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
+		sum = sum + va_arg(ap, int);
 	va_end(ap);
 
 	return (sum);
