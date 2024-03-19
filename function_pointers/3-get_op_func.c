2613141ee6268376e5618a85f10fb14f720101f0
AxelNAY <axelnay@sfr.fr>
2024-03-19 10:11:57 +0000

    fourth C program

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -11,16 +11,22 @@
 
 int (*get_op_func(char *s))(int a, int b)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
+	i = 0;
+	while (ops[i] != NULL)
+	{
+		if (s = ops[i])
+			return (ops[i]);
+	}
 
-    return (NULL);
+	return (NULL);
 }
