832919ed70797c48b53cc37f63db613df3769a99
AxelNAY <axelnay@sfr.fr>
2024-03-20 13:28:54 +0000

    fourth C program

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,6 +4,46 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - print a character.
+ * @c: character.
+ */
+
+void print_char(char c)
+{
+	printf("%c", c);
+}
+
+/**
+ * print_int - print an integer.
+ * @i: integer.
+ */
+
+void print_int(int i)
+{
+	printf("%d", i);
+}
+
+/**
+ * print_float - print a float.
+ * @f: float.
+ */
+
+void print_float(float f)
+{
+	printf("%f", f);
+}
+
+/**
+ * print_string - print a string.
+ * @str: string.
+ */
+
+void print_string(char *str)
+{
+        printf("%s", str);
+}
+
+/**
  * print_all - prints anything.
  * @format: format.
  * @...: variables parameters.
@@ -14,27 +54,28 @@ void print_all(const char * const format, ...)
 	va_list ap;
 	int i = 0;
 	int j;
+	char c;
+	int i;
+	float f;
 	char *str;
-	char *s = {'c', 'i', 'f', 's'};
-	char *inclu = {"%c", "%d", "%f", "%s"};
+	char s[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
 	char *list = {"char", "int", "float", "char *"}
 
 	va_start(ap, format);
-	while (format[i])
+	while (format[i] != NULL)
 	{
 		j = 0;
 		while (s[j])
 		{
-			if (format[i] == s[j])
-			{
-				str = va_arg(ap, list[i]);
-				printf("%s", inclu[j], str);
-			}
-			j++;
+			if (format[i] == s[j].s[0])
+				return (ops[i].f);
 		}
-		if (format[i++] == NULL)
-			printf(", ");
-		i++;
 	}
 	printf("\n");
 	va_end(ap);
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef variadic_functions_h
 #define variadic_functions_h
 
+/**
+ * struct pr - Struct pr
+ * @pr: Type
+ * @f: The function associated
+ */
+
+typedef struct pr
+{
+	char *pr;
+	void (*f)(char c);
+} pr_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
