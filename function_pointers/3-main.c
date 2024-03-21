b1031f291a11bf0974f9b9190ad4df9da620edef
AxelNAY <axelnay@sfr.fr>
2024-03-21 08:24:49 +0000

    update 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -17,19 +17,19 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
 	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		exit(99);
+		return (99);
 	}
 
 	o = get_op_func(argv[2]);
 	if (o == NULL)
 	{
 		printf("Error\n");
-		exit(99);
+		return (99);
 	}
 
 	a = atoi(argv[1]);
