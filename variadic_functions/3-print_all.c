e138f0e54135010adb1dffa3bd627870609d4dff
AxelNAY <axelnay@sfr.fr>
2024-03-22 13:22:06 +0000

    betty 3-print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -45,7 +45,7 @@ void print_string(va_list ap)
 		printf("(nil)");
 		return;
 	}
-        printf("%s", va_arg(ap, char *));
+	printf("%s", va_arg(ap, char *));
 }
 
 /**
@@ -64,7 +64,8 @@ void print_all(const char * const format, ...)
 		{'c', print_char},
 		{'i', print_int},
 		{'f', print_float},
-		{'s', print_string}
+		{'s', print_string},
+		{NULL, NULL}
 	};
 
 	va_start(ap, format);
