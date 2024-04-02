bf8551a27f0dbacddc13a3bca0647bde17183a7e
AxelNAY <axelnay@sfr.fr>
2024-04-02 13:10:04 +0000

    Update 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,11 +9,25 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	int len = 0;
+	unsigned int i, len = 0;
 	list_t *new;
 
-	while (str)
+	for (i = 0; str[i] != '\0'; i++)
 		len++;
 
-	printf("[%u] %s\n", h->len, h->str);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = len;
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
