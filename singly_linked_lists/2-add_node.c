ea34d7f3150ff4c74056221e70cc9a36dce3158b
AxelNAY <axelnay@sfr.fr>
2024-04-02 09:45:34 +0000

    Update 2-add_node.c

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: place.
+ * @str: name of the new node.
+ * Return: size.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int len = 0;
+	list_t *new;
+
+	while (str)
+		len++;
+
+	printf("[%u] %s\n", h->len, h->str);
+}
