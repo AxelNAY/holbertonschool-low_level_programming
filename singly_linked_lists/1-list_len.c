920eb4217aacd8e161e741d687372fed1de42bc9
AxelNAY <axelnay@sfr.fr>
2024-04-02 09:09:32 +0000

    complete task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: list.
+ * Return: number of elements in a linked list_t list. 
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		size++;
+		h = h->next;
+	}
+	return (size);
+}
+
