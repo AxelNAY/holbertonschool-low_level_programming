cce7e2a7e580c4701c3aa22d65ba5cfdcc42ac0f
AxelNAY <axelnay@sfr.fr>
2024-04-03 21:16:25 +0000

    betty checked 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t linked list.
+ * delete_dnodeint_at_index - deletes the node
+ * at index index of a dlistint_t linked list.
  * @head: double pointer to the list
  * @index: index
  * Return: 1 if succeeded or -1 if failed
