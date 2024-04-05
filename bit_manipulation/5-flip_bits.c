ebc0c3feb657461fb0122487d36e4964e28df69f
AxelNAY <axelnay@sfr.fr>
2024-04-05 20:42:04 +0000

    Complete the file 5-flip_bits.c

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * set_bit - sets the value of a bit to 1 at a given index.
+ * set_bit - returns the number of bits you would need
+ * to flip to get from one number to another.
  * @n: number.
- * @index: index.
- * Return: 1 if success or -1 if fail.
+ * @m: the other number.
+ * Return: the number of bits.
  */
 
-int set_bit(unsigned long int *n, unsigned int index)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	if (index > 63)
-		return (-1);
+	unsigned int numb = (n & 1) ^ (m & 1);
 
-	*n = ((1UL << index) | *n);
-	return (1);
+	if (n == m)
+		return (numb);
+	
+	return (numb + flip_bits(n >> 1, m >> 1));
 }
