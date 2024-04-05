0169425f34964bf11355632e4edb665366611dd2
AxelNAY <axelnay@sfr.fr>
2024-04-05 10:23:07 +0000

    Update the file 1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -8,32 +8,42 @@
 
 unsigned long int print_f5(unsigned long int n)
 {
-        unsigned long int test, pair, dec;
+	unsigned long int test, pair, dec;
 
-        if (n > 9999)
-        {
-                dec = n * 0.0001;
-                pair = dec;
-                test = dec * 0.2;
-                if (pair == (test * 2))
-                        _putchar('0');
-                else
-                        _putchar('1');
-                n = n - (dec * 10000);
-        }
-	if (n > 999)
+	if (n > 999999999)
 	{
-		dec = n * 0.001;
+		dec = n * 0.000000001;
 		pair = dec;
 		test = dec * 0.2;
 		if (pair == (test * 2))
 			_putchar('0');
 		else
 			_putchar('1');
-		n = n - (dec * 1000);
+		n = n - (dec * 1000000000);
+	}
+	if (n > 99999999)
+	{
+		dec = n * 0.00000001;
+		pair = dec;
+		test = dec * 0.2;
+		if (pair == (test * 2))
+			_putchar('0');
+		else
+			_putchar('1');
+		n = n - (dec * 100000000);
+	}
+	if (n > 9999999)
+	{
+                dec = n * 0.000001;
+                pair = dec;
+                test = dec * 0.2;
+                if (pair == (test * 2))
+                        _putchar('0');
+                else
+                        _putchar('1');
 	}
 	if (n > 999999)
-	{
+        {
                 dec = n * 0.01;
                 pair = dec;
                 test = dec * 0.2;
@@ -41,7 +51,7 @@ unsigned long int print_f5(unsigned long int n)
                         _putchar('0');
                 else
                         _putchar('1');
-	}
+        }
 
 	dec = n * 0.1;
 	pair = dec;
