92d401e3dd2b113f7e1b0e254a86646f6fa49f2c
AxelNAY <axelnay@sfr.fr>
2024-04-05 08:58:58 +0000

    Complete the file 0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,5 +9,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	int i;
+	unsigned int dec = 0;
 
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec = 2 * dec + (b[i] - '0');
+	}
+
+	return (dec);
 }
