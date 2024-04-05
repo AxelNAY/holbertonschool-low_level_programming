cb58230086efa86117aecaed4f25fd5c3e03d941
AxelNAY <axelnay@sfr.fr>
2024-04-05 13:48:40 +0000

    Complete the file 2-get_bit.c

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -15,8 +15,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-unsigned long int print_mil(unsigned long int n);
-unsigned long int print_million(unsigned long int n);
-unsigned long int print_milliard(unsigned long int n);
 
 #endif
