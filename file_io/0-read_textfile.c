c2332b67d247d207b7df0aaf74d7714512e6da22
AxelNAY <axelnay@sfr.fr>
2024-04-06 14:53:57 +0000

    Creation of all the files and complete the file 0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * @filename: name of the file.
+ * @letters: the number of letters it should read and print.
+ * Return: the actual number of letters it could read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int op, rd_let, numb;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	if (op < 0)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	rd_let = read(op, buffer, letters);
+	if (rd_let < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	buffer[rd_let] = '\0';
+	close(op);
+
+	numb = write(STDOUT_FILENO, buffer, rd_let);
+	if (numb < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	return (numb);
+}
